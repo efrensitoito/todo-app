6c126a1eb7a92c6b7501b64bd75de4b4f0516c89
Efren <efrensitoito@gmail.com>
2025-12-17 15:26:31 -0400

    test ok with tets endpoint health!

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -10,6 +10,15 @@ const app = express();
 app.use(cors());
 app.use(express());
 
+// Health check endpoint
+app.get('/health', async (req,res) => {
+    try {
+        res.status(200).json({ status: 'ok', message: 'Server and DB Running!'})
+    } catch (error) {
+        res.status(500).json({ status: 'ok', message: 'Server with no health!!'})
+    }
+});
+
 // Routes
 // ------
 
