d0c48ae10471a62c7795fbe65952de8a4a4b1b2b
Efren <efrensitoito@gmail.com>
2025-12-17 21:26:10 -0400

    Task controller created!

---
+++ backend/src/controllers/task.controller.ts
@@ -0,0 +1,86 @@
+import { Request, Response } from "express";
+import { TaskService } from "../services/task.service";
+
+export class TaskController {
+  static async createTask(req: Request, res: Response) {
+    try {
+      const { title, description } = req.body;
+      const userId = req.user?.Id;
+
+      if (!title) {
+        return res.status(400).json({ message: "Title is required" });
+      }
+
+      const task = await TaskService.createTask({
+        title,
+        description,
+        userId,
+      });
+
+      return res.status(201).json(task);
+    } catch (error) {
+      return res.status(500).json({ message: "Error creating task" });
+    }
+  }
+
+  static async getTasks(req: Request, res: Response) {
+    try {
+      const userId = req.user?.Id;
+      const tasks = await TaskService.getTasksByUser(userId);
+      return res.status(200).json(tasks);
+    } catch (error) {
+      return res.status(500).json({ message: "Error fetching tasks" });
+    }
+  }
+
+  static async updateTask(req: Request, res: Response) {
+    try {
+      const { id } = req.params;
+      const userId = req.user?.Id;
+
+      const task = await TaskService.updateTask(id, userId, req.body);
+
+      if (!task) {
+        return res.status(404).json({ message: "Task not found" });
+      }
+
+      return res.status(200).json(task);
+    } catch (error) {
+      return res.status(500).json({ message: "Error updating Task" });
+    }
+  }
+
+  static async deleteTask(req: Request, res: Response) {
+    try {
+      const { id } = req.params;
+      const userId = req.user?.Id;
+
+      const deleted = await TaskService.deleteTask(id, userId);
+
+      if (!deleted) {
+        return res.status(404).json({ message: "Task not found" });
+      }
+
+      return res.status(204).send();
+    } catch (error) {
+      return res.status(500).json({ message: "Error deleting task!!" });
+    }
+  }
+
+  static async markAsCompleted(req: Request, res: Response) {
+    try {
+        const { id } = req.params;
+        const userId = req.user?.Id;
+
+        const task = await TaskService.markAsCompleted(id, userId, req.body);
+
+        if(!task) {
+            return res.status(404).json({ message: 'Task not found' });
+        }
+
+        return res.status(200).json(task);
+    } catch (error) {
+      return res.status(500).json({ message: "Error marking task as completed!!" });
+    }
+  }
+}
